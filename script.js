2f4b82e11e3eda83c9a112c3cfbfb53d50fc0a55
cor3man <lisockij@gmail.com>
2023-07-22 02:10:37 +0200

    fix reload issue, refactor

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
 function drawChart() {
-  const ucs = parseFloat(document.getElementById("ucs-input").value);
-  const hgb = parseFloat(document.getElementById("hgb-input").value);
-  const cci = parseFloat(document.getElementById("cci-input").value);
-
-  const inputData = {
-    ucs: ucs,
-    hgb: hgb,
-    cci: cci
-  }
-
-  const logits = [clavien(-2.94, inputData), clavien(-1.35, inputData), clavien(-0.1, inputData), clavien(1.09, inputData)]
-  const probs = [
-    logits[0],
-    logits[1] - logits[0],
-    logits[2] - logits[1],
-    logits[3] - logits[2],
-    1 - logits[3]
-  ];
-
-  const ctx = document.getElementById("myChart").getContext("2d");
-
+  document.querySelector("#chart-wrapper").innerHTML = '<canvas id="clavienChart"></canvas>';
+  const ctx = document.getElementById("clavienChart").getContext("2d");
   const labels = ['Clavien 1', 'Clavien 2', 'Clavien 3', 'Clavien 4', 'Clavien 5'];
+
+  const probs = calcProbabilities();
+
   const data = {
     labels: labels,
     datasets: [{
       label: 'P (Clavien Dindo)',
-      data: probs,//[65, 59, 80, 81, 56],
+      data: probs,
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0.1
@@ -37,15 +21,42 @@ function drawChart() {
     data: data,
   };
 
-  const myChart = new Chart(ctx, {
+  const chart = new Chart(ctx, {
     type: "line",
     data: data,
     config: config
     //options: options,
   });
+
+  //chart.destroy();
 }
 
-function clavien(b1, inputData) {
-  const logit = b1 - 0.18*inputData.cci  - (-0.22 * inputData.hgb) - (-1.54 * inputData.ucs);
+function calcProbabilities() {
+  const ucs = parseFloat(document.getElementById("ucs-input").value);
+  const hgb = parseFloat(document.getElementById("hgb-input").value);
+  const cci = parseFloat(document.getElementById("cci-input").value);
+
+  const inputData = {
+    ucs: ucs,
+    hgb: hgb,
+    cci: cci
+  };
+
+  const b0 = [-2.94, -1.35, -0.1, 1.09] 
+  const clProbs = b0.map(b0 => clavienProbability(b0, inputData));
+
+  const probs = [
+    clProbs[0],
+    clProbs[1] - clProbs[0],
+    clProbs[2] - clProbs[1],
+    clProbs[3] - clProbs[2],
+    1 - clProbs[3]
+  ];
+
+  return probs;
+} 
+
+function clavienProbability(b1, inputData) {
+  const logit = b1 - 0.18 * inputData.cci  - (-0.22 * inputData.hgb) - (-1.54 * inputData.ucs);
   return Math.exp(logit)/(1 + Math.exp(logit))
 }
