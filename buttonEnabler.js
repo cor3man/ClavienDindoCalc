749934870d0327740d834ea81bcdd5f0357ca99f
unknown <INV-92PL-L$@godeltech.com>
2023-07-27 17:52:19 +0200

    add validations

--- buttonEnabler.js
+++ buttonEnabler.js
@@ -1,33 +1,51 @@
-// Get references to the input fields and the submit button
 const field1Input = document.getElementById('ucs-input');
 const field2Input = document.getElementById("hgb-input");
 const field3Input = document.getElementById("cci-input");
 const submitButton = document.getElementById("submitButton");
 
-// Add event listeners to the input fields to check for changes
-//field1Input.addEventListener('input', checkFieldsAndEnableButton);
-field2Input.addEventListener('input', validateHGB);
-//field2Input.addEventListener('input', checkFieldsAndEnableButton);
-//field3Input.addEventListener('input', checkFieldsAndEnableButton);
+field2Input.addEventListener('focusout', validateHGB);
+field3Input.addEventListener('focusout', validateCCI);
+field1Input.addEventListener('input', checkFieldsAndEnableButton);
+field2Input.addEventListener('input', checkFieldsAndEnableButton);
+field3Input.addEventListener('input', checkFieldsAndEnableButton);
 
 function validateHGB() {
-    const field2Input = document.getElementById("hgb-input");
-    const value = field2Input.value.trim()
-    field2Input.value = field2Input.value.replace(/[^0-9]/g, "");
+    const fieldInput = document.getElementById("hgb-input");
+    const fieldInputLabelError = document.getElementById("hgb-error-label");
+    const value = fieldInput.value.trim()
 
-    
+    if (value < 5 || value > 20) {
+        fieldInputLabelError.textContent = " - Niewłaściwa wartość";
+        console.log("error!!!!");
+    }
+    else fieldInputLabelError.textContent = "";   
 
+    checkFieldsAndEnableButton();
     console.log(value);
 }
 
-// Function to check if all input fields have values and enable the button
+function validateCCI() {
+    const fieldInput = document.getElementById("cci-input");
+    const fieldInputLabelError = document.getElementById("cci-error-label");
+    const value = fieldInput.value.trim()
+
+    if (value < 1 || value > 37) {
+        fieldInputLabelError.textContent = " - Niewłaściwa wartość";
+    }
+    else fieldInputLabelError.textContent = "";   
+
+    checkFieldsAndEnableButton();
+}
+
 function checkFieldsAndEnableButton() {
     const field1Value = field1Input.value.trim();
     const field2Value = field2Input.value.trim();
     const field3Value = field3Input.value.trim();
+    const field2InputLabelText = document.getElementById("cci-error-label").textContent;
+    const field3InputLabelText = document.getElementById("hgb-error-label").textContent;
 
-    // Enable the button only if all fields have values
-    if (field1Value !== '' && field2Value !== '' && field3Value !== '') {
+    if (field1Value !== '' && field2Value !== '' && field3Value !== ''
+        && field2InputLabelText == '' && field3InputLabelText == '') {
         submitButton.removeAttribute('disabled');
     } else {
         submitButton.setAttribute('disabled', true);
